e4952da627a1416f1670f037bb1706596f867e18
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-08-18 23:52:42 +0900

    deleteFirebase

--- screens/Main/TempHome.js
+++ screens/Main/TempHome.js
@@ -3,7 +3,6 @@ import {StyleSheet, View, Text, Button} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import {useDispatch} from 'react-redux';
 import {logOut} from '../../redux/usersSlice';
-
 // https://github.com/streamich/react-use
 
 const TempHome = () => {
@@ -81,70 +80,6 @@ const TempHome = () => {
     </View>
   );
 };
-
-// class TempHome extends Component {
-//   render() {
-//     return (
-//       <View
-//         style={{
-//           flex: 1,
-//           alignItems: 'center', // 가로 정렬
-//           justifyContent: 'center', // 세로 정렬
-//           paddingVertical: 100,
-//         }}>
-//         <Button
-//           style={styles.buttonStyle}
-//           title="매칭방 만들기"
-//           onPress={() => {
-//             navigation.navigate('MakeMatching');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="매칭 요청중 - Host"
-//           onPress={() => {
-//             navigation.navigate('MatchingRequestHost');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="매칭 요청하기 - Client"
-//           onPress={() => {
-//             navigation.navigate('MatchingRequestClient');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="매칭 성공"
-//           onPress={() => {
-//             navigation.navigate('MatchingSuccess');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="매칭 실패"
-//           onPress={() => {
-//             navigation.navigate('MatchingFailed');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="공지사항"
-//           onPress={() => {
-//             navigation.navigate('InfoBoard');
-//           }}
-//         />
-//         <Button
-//           style={styles.buttonStyle}
-//           title="약관 및 정책"
-//           onPress={() => {
-//             navigation.navigate('PoliciesBoard');
-//           }}
-//         />
-//       </View>
-//     );
-//   }
-// }
 
 const styles = StyleSheet.create({
   buttonStyle: {
--- screens/Main/TempSendMsg.js
+++ screens/Main/TempSendMsg.js
@@ -2,21 +2,11 @@ import React, {Component, useEffect, useState} from 'react';
 import {StyleSheet, View, Text, Button, Linking, TextInput} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 import Styles from '../../assets/Styles2';
-// import {dbService, sendData} from '../../firebase';
-import {dbService, sendData} from '../../rn-firebase';
 
 const TempSendMsg = () => {
   const navigation = useNavigation();
   const [text, setText] = useState('');
   const [texts, setTexts] = useState('empty haha');
-
-  //   const getNweets = async () => {
-  //     const dbNweets = dbService.collection('nweets').get();
-  //     alert(dbNweets);
-  //   };
-  //   //   useEffect(() => {
-  //   //     getNweets();
-  //   //   });
 
   return (
     <View style={Styles.centerize}>
@@ -29,7 +19,6 @@ const TempSendMsg = () => {
       <Button
         title="전송"
         onPress={() => {
-          sendData(text);
           setText('');
         }}
       />
