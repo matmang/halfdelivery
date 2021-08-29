bc55bf17db972522d5a1a165a3dba1ac6877b248
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-08-29 23:28:02 +0900

    First commit from renew_main

--- api.js
+++ api.js
@@ -1,19 +1,20 @@
 import axios from "axios";
-import qs from 'query-string'
+import qs from "query-string";
 
 const callApi = async (method, path, data, jwt) => {
   const headers = {
     Authorization: jwt,
     "Content-Type": "application/json",
   };
-  const baseUrl = "http://0c53-211-207-141-139.ngrok.io/v1";
+  // const baseUrl = "http://0c53-211-207-141-139.ngrok.io/v1";
+  const baseUrl = "http://61af-211-207-141-139.ngrok.io/v1";
   const fullUrl = `${baseUrl}${path}`;
-  if(method === "get" || method === "delete") {
-      return axios[method](fullUrl, {headers});
+  if (method === "get" || method === "delete") {
+    return axios[method](fullUrl, { headers });
   } else {
-      return axios[method](fullUrl, data, {headers});
+    return axios[method](fullUrl, data, { headers });
   }
 };
 
-export const createAccount = form => callApi("post", "/users/", form);
-export const login = form => callApi("post", "/users/login/", form);
+export const createAccount = (form) => callApi("post", "/users/", form);
+export const login = (form) => callApi("post", "/users/login/", form);
--- components/Gate.js
+++ components/Gate.js
@@ -1,16 +1,16 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import Auth from '../navigations/Auth';
-import Main from '../navigations/Main';
-import {logOut} from '../redux/usersSlice';
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Auth from "../navigations/Auth";
+import Main from "../navigations/Main";
+import { logOut } from "../redux/usersSlice";
 
 export default () => {
-  const {isLoggedIn} = useSelector(state => state.usersReducer);
+  const { isLoggedIn } = useSelector((state) => state.usersReducer);
   const dispatch = useDispatch();
   return (
     <NavigationContainer>
-      {!isLoggedIn ? <Main /> : <Auth />}
+      {isLoggedIn ? <Main /> : <Auth />}
     </NavigationContainer>
   );
 };
