c3a1b9c59d894b186cb75bb635ba6010c9da0a32
matmang <64837797+matmang@users.noreply.github.com>
2021-09-20 15:22:02 +0900

    conflict fix 3

--- navigations/Main.js
+++ navigations/Main.js
@@ -1,82 +1,107 @@
-import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+  
+// import React from "react";
+// import { createStackNavigator } from "@react-navigation/stack";
 
-import TempHome from '../screens/Main/TempHome';
-import MakeMatching from '../screens/Main/MakeMatching';
-import MatchingRequestClient from '../screens/Main/MatchingRequestClient';
-import MatchingRequestHost from '../screens/Main/MatchingRequestHost';
-import MatchingSuccess from '../screens/Main/MatchingSuccess';
-import MatchingFailed from '../screens/Main/MatchingFailed';
-import InfoBoard from '../screens/Main/InfoBoard';
-import PoliciesBoard from '../screens/Main/PoliciesBoard';
-import TempSendMsg from '../screens/Main/TempSendMsg';
+// import TempHome from "../screens/Main/TempHomeScreen";
+// import MatchingList from "../screens/Main/MatchingListScreen";
 
-const Main = createStackNavigator();
+// import MatchingReqClient from "../screens/Main/MatchingReqClientScreen";
+// import MatchingReqHost from "../screens/Main/MatchingReqHostScreen";
+// import MatchingSuccess from "../screens/Main/MatchingSuccessScreen";
+// import MatchingFailed from "../screens/Main/MatchingFailedScreen";
+// import InfoBoardScreen from "../screens/Main/InfoBoardScreen";
+// import PoliciesBoard from "../screens/Main/PoliciesBoardScreen";
+// import TempSendMsg from "../screens/Main/TempSendMsgScreen";
+// import SetMatchingTime from "../screens/Main/SetMatchingTimeScreen";
 
-export default () => (
-  <Main.Navigator initialRouteName="TempHome" screenOptions={{}}>
-    <Main.Screen
-      name="TempHome"
-      component={TempHome}
-      options={{
-        title: '(임시) 홈',
-      }}
-    />
-    <Main.Screen
-      name="TempSendMsg"
-      component={TempSendMsg}
-      options={{
-        title: '메시지 테스트',
-      }}
-    />
-    <Main.Screen
-      name="MakeMatching"
-      component={MakeMatching}
-      options={{
-        title: '매칭방 만들기',
-      }}
-    />
-    <Main.Screen
-      name="MatchingRequestHost"
-      component={MatchingRequestHost}
-      options={{
-        title: '매칭 요청중 - Host',
-      }}
-    />
-    <Main.Screen
-      name="MatchingRequestClient"
-      component={MatchingRequestClient}
-      options={{
-        title: '매칭 요청하기 - Client',
-      }}
-    />
-    <Main.Screen
-      name="MatchingSuccess"
-      component={MatchingSuccess}
-      options={{
-        title: '매칭 성공',
-      }}
-    />
-    <Main.Screen
-      name="MatchingFailed"
-      component={MatchingFailed}
-      options={{
-        title: '매칭 실패',
-      }}
-    />
-    <Main.Screen
-      name="InfoBoard"
-      component={InfoBoard}
-      options={{
-        title: '공지사항',
-      }}
-    />
-    <Main.Screen
-      name="PoliciesBoard"
-      component={PoliciesBoard}
-      options={{
-        title: '약관 및 정책',
-      }}
-    />
-  </Main.Navigator>
-);
+// const Main = createStackNavigator();
+// const Chat = createStackNavigator();
+
+// export default () => (
+//   <Main.Navigator initialRouteName="TempHome" screenOptions={{}}>
+//     <Main.Screen
+//       name="TempHome"
+//       component={TempHome}
+//       options={{
+//         title: "(임시) 홈",
+//       }}
+//     />
+//     <Main.Screen
+//       name="SetMatchingTime"
+//       component={SetMatchingTime}
+//       options={{
+//         title: "매칭 시간 설정",
+//       }}
+//     />
+//     {/* <Chat.Screen
+//       name="Channel"
+//       component={Channel}
+//       options={{
+//         title: "채널",
+//       }}
+//     /> */}
+//     <Main.Screen
+//       name="TempSendMsg"
+//       component={TempSendMsg}
+//       options={{
+//         title: "메시지 테스트",
+//       }}
+//     />
+//     <Main.Screen
+//       name="MatchingList"
+//       component={MatchingList}
+//       options={{
+//         title: "매칭 리스트",
+//       }}
+//     />
+//     <Main.Screen
+//       name="CreateMatching"
+//       component={CreateMatchingScreen}
+//       options={{
+//         title: "매칭방 만들기",
+//       }}
+//     />
+//     <Main.Screen
+//       name="MatchingReqHost"
+//       component={MatchingReqHost}
+//       options={{
+//         title: "매칭 요청중 - Host",
+//       }}
+//     />
+//     <Main.Screen
+//       name="MatchingReqClient"
+//       component={MatchingReqClient}
+//       options={{
+//         title: "매칭 요청하기 - Client",
+//       }}
+//     />
+//     <Main.Screen
+//       name="MatchingSuccess"
+//       component={MatchingSuccess}
+//       options={{
+//         title: "매칭 성공",
+//       }}
+//     />
+//     <Main.Screen
+//       name="MatchingFailed"
+//       component={MatchingFailed}
+//       options={{
+//         title: "매칭 실패",
+//       }}
+//     />
+//     <Main.Screen
+//       name="InfoBoard"
+//       component={InfoBoardScreen}
+//       options={{
+//         title: "공지사항",
+//       }}
+//     />
+//     <Main.Screen
+//       name="PoliciesBoard"
+//       component={PoliciesBoard}
+//       options={{
+//         title: "약관 및 정책",
+//       }}
+//     />
+//   </Main.Navigator>
+// );
