b796e8fe85ffc3f323e9f42a4d3de91ed37d6072
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-10-10 23:33:56 +0900

    Add catch(err)

--- screens/Main/StoreStack/SetMatchingTimeScreen.js
+++ screens/Main/StoreStack/SetMatchingTimeScreen.js
@@ -15,6 +15,7 @@ const SetMatchingTimeScreen = (props) => {
   const [orderID, setOrderID] = useState("/");
   // const [chatRoomID, setChatRoomID] = useState("#"); // ! useState 사용시, createChatRoom() 함수 async-await 안에서 예상대로 작동하지 않는다...
   let chatRoomID = "#";
+  let authUser = {};
 
   const storeName = useSelector((state) => state.orderReducer.storeName);
   const menus = useSelector((state) => state.orderReducer.menus);
@@ -31,6 +32,19 @@ const SetMatchingTimeScreen = (props) => {
 
   const QUANTITY = -1; //! 임시값.
   const PAYMENT_AMOUNT = -1000; //! 임시값.
+
+  useEffect(() => {
+    fetchAuthUser();
+  }, []);
+
+  const fetchAuthUser = async () => {
+    const _authUser = await Auth.currentAuthenticatedUser().catch((error) => {
+      console.log("_authUser", error);
+    });
+
+    authUser = _authUser;
+    console.log("authUser", authUser);
+  };
 
   // useEffect(() => {
   //   createOrder();
@@ -58,15 +72,19 @@ const SetMatchingTimeScreen = (props) => {
     console.log("chatRoomID", chatRoomID);
 
     // ? Authenticated User 와 ChatRoom 을 연결하기.
-    const authUser = await Auth.currentAuthenticatedUser();
+    // const authUser = await Auth.currentAuthenticatedUser();
     // ? DataStore 의 User 모델에서 authUser.attributes.sub 값과 일치하는 값만 가져온다.
-    const dbAuthUser = await DataStore.query(User, authUser.attributes.sub);
+    const dbAuthUser = await DataStore.query(User, authUser.attributes.sub).catch((error) => {
+      console.log("dbAuthUser", error);
+    });
     await DataStore.save(
       new ChatRoomUser({
         user: dbAuthUser,
         chatroom: newChatRoom,
       })
-    );
+    ).catch((error) => {
+      console.log("ChatRoomUser", error);
+    });
 
     await createOrder();
 
@@ -80,7 +98,7 @@ const SetMatchingTimeScreen = (props) => {
 
   // ? 호스트가 고른 식당으로, Order 생성.
   const createOrder = async () => {
-    const authUser = await Auth.currentAuthenticatedUser();
+    // const authUser = await Auth.currentAuthenticatedUser();
     const msTime = Date.now();
     const AWSDateTime = new Date(msTime).toISOString();
     const newOrder = await DataStore.save(
@@ -91,11 +109,15 @@ const SetMatchingTimeScreen = (props) => {
         userID: authUser.attributes.sub,
         chatroomID: chatRoomID,
       })
-    );
+    ).catch((error) => {
+      console.log("newOrder", error);
+    });
     setOrderID(newOrder.id);
 
     const fetchedOrders = await DataStore.query(Order, (order) => order.userID("eq", authUser.attributes.sub), {
       sort: (s) => s.orderDate(SortDirection.DESCENDING),
+    }).catch((error) => {
+      console.log("fetchedOrders", error);
     });
     console.log("fetchedOrders", fetchedOrders);
     const fetchOrderID = fetchedOrders[0].id;
@@ -106,7 +128,7 @@ const SetMatchingTimeScreen = (props) => {
 
   // ? 호스트의 메뉴로 OrderMenu 생성.
   const createOrderMenu = async () => {
-    const authUser = await Auth.currentAuthenticatedUser();
+    // const authUser = await Auth.currentAuthenticatedUser();
     const newOrderMenu = await DataStore.save(
       new OrderMenu({
         menu: firstMenu.menu,
@@ -115,7 +137,9 @@ const SetMatchingTimeScreen = (props) => {
         userID: authUser.attributes.sub,
         orderID: orderID,
       })
-    );
+    ).catch((error) => {
+      console.log("newOrderMenu", error);
+    });
     // ! 계정 imageUri 가 비워져 있으면, 왠진 모르겠지만, 새 채팅방으로 이동 하지 않는다.
     navigation.navigate("ChatRoomScreen", {
       chatRoomID: chatRoomID,
