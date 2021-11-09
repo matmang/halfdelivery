0021250538cfaced1def2352be1eeb45de250d51
TomTimmy <70505699+TomTimmy@users.noreply.github.com>
2021-11-10 00:46:53 +0900

    Style MatchingListScreen
    Just styled..
    No functions work.

--- navigations/router/BttmTabStacks/btPartnerStack.js
+++ navigations/router/BttmTabStacks/btPartnerStack.js
@@ -1,25 +1,36 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-
 import { View, Image, Text, SafeAreaView, Button } from "react-native";
-
 import logos from "../../../images";
-
+import styled from "styled-components";
+import colors from "../../../colors";
 import MatchingListScreen from "../../../screens/Main/PartnerStack/MatchingListScreen";
 
 const Stack = createStackNavigator();
-const LogoHeader = (props) => {
+
+const HeaderView = styled.SafeAreaView`
+  margin-top: 24px;
+  justify-content: center;
+  align-items: center;
+  background-color: ${colors.mainBlue};
+  height: 56px;
+  width: 100%;
+  /* position: relative; */
+`;
+
+const Title = styled.Text`
+  font-family: "noto-regular";
+  font-size: 20px;
+  color: #ffffff;
+  font-weight: bold;
+`;
+
+const BlueHeader = (props) => {
+  console.log(props);
   return (
-    <SafeAreaView
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-      }}
-    >
-      <Image source={logos.halfLogo} style={{ width: 40, height: 40 }} />
-      {/* <Text>{props.title}</Text> */}
-    </SafeAreaView>
+    <HeaderView>
+      <Title>파트너 리스트</Title>
+    </HeaderView>
   );
 };
 
@@ -27,14 +38,13 @@ export default () => (
   <Stack.Navigator
     initialRouteName="MatchingListScreen"
     screenOptions={{
-      header: (props) => <LogoHeader {...props} />,
+      header: (props) => <BlueHeader {...props} />,
     }}
   >
     <Stack.Screen
       name="MatchingListScreen"
       component={MatchingListScreen}
       options={{
-        header: (props) => <LogoHeader {...props} />,
         title: "매칭 리스트",
       }}
     />
--- screens/Main/StoreStack/SelectStoreScreen.js
+++ screens/Main/StoreStack/SelectStoreScreen.js
@@ -25,12 +25,11 @@ import QuantitySelector from "../../../components/Matching/QuantitySelector";
 import styled from "styled-components";
 import colors from "../../../colors";
 
-const ButtonTitle = styled.Text`
-  font-family: "noto-regular";
-  font-size: 17px;
-  line-height: 20px;
-  color: ${colors.blueGrey};
+const Categorries = styled.Pressable`
+  height: 52px;
+  justify-content: center;
 `;
+
 const SelectStoreScreen = () => {
   //
   const KOREAN_ID = "8314af0c-1d8e-4112-869b-15689debb495";
@@ -46,16 +45,18 @@ const SelectStoreScreen = () => {
     setCategoryID("ALL");
   }, []);
 
-  useEffect(() => {
-    console.log("categoryID", categoryID);
-    // setCategoryID("all");
-    // fetchStores();
-  }, [categoryID]);
+  const ButtonTitle = styled.Text`
+    font-family: "noto-regular";
+    font-size: 17px;
+    /* line-height: 20px; */
+    color: ${({ id }) =>
+      id === categoryID ? colors.mainBlue : colors.blueGrey};
+  `;
+
   //! 디자인 시안 나오기 전까진, 일단 Button 으로...
   const CategoryButton = (props) => {
     return (
-      <Pressable
-        style={{ margin: 10 }}
+      <Categorries
         onPress={() => {
           switch (props.name) {
             case "한식":
@@ -79,8 +80,8 @@ const SelectStoreScreen = () => {
           }
         }}
       >
-        <ButtonTitle>{props.name}</ButtonTitle>
-      </Pressable>
+        <ButtonTitle id={props.id}>{props.name}</ButtonTitle>
+      </Categorries>
     );
   };
 
@@ -98,11 +99,11 @@ const SelectStoreScreen = () => {
       </View> */}
 
       <View style={styles.categoryButtons}>
-        <CategoryButton name="한식" />
-        <CategoryButton name="중식" />
-        <CategoryButton name="일식" />
-        <CategoryButton name="양식" />
-        <CategoryButton name="카페" />
+        <CategoryButton name="한식" id={KOREAN_ID} />
+        <CategoryButton name="중식" id={CHINESE_ID} />
+        <CategoryButton name="일식" id={JAPANESE_ID} />
+        <CategoryButton name="양식" id={WESTERN_ID} />
+        <CategoryButton name="카페" id={CAFE_ID} />
       </View>
 
       <StoreList categoryID={categoryID} />
